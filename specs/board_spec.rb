3940e75fcbad0769c7c2fa7e358711a06bfcf5cd
Raul Alejandro Gonzalez <Nawn1994@gmail.com>
2016-03-30 23:44:43 -0700

    Delete the tests for the display
    I deleted the tests for the table/display on the board. That is not as
    important as figuring out how to organize/have the board rows be in the
    Board class. Need to decide on that first

--- specs/board_spec.rb
+++ specs/board_spec.rb
@@ -76,10 +76,6 @@ describe Board do
       end
     end
 
-    it "contains an empty board" do
-      @board = Board.new
-      expect(@board.table).to be_instance_of(Board)
-      expect(@board.table).to eql(example())
-    end
+    #Create tests for rows containing pieces
   end
 end
