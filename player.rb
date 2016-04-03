a7d986d42b22eed2d81dd3c9cb5dc207970dbf52
Raul Alejandro Gonzalez <Nawn1994@gmail.com>
2016-04-02 21:47:05 -0700

    Add Player#select to select board_piece

--- player.rb
+++ player.rb
@@ -8,6 +8,23 @@ class Player
     @team_color = team_sym
     @board = host_board
   end
+
+  def select(input_string)
+    row, pos = coord_string(input_string)
+    piece = @board.rows[row][pos]
+    raise StandardError.new("Space is empty") if piece == " "
+    raise StandardError.new("Not your piece") unless piece.team_color == @team_color
+    piece
+  end
+
+  private
+  def coord_string(input_string)
+    raise ArgumentError.new("Input must be String") unless input_string.is_a? String
+    input_string = input_string.gsub(/\s+/, "").upcase
+    raise ArgumentError.new("Input incorrect. Example: C4, B2, A3, etc.") unless input_string =~ /[A-H][1-8]/
+
+    [8-input_string[1].to_i, input_string[0].ord - 65]
+  end
 end
 
 class Artificial < Player; end
