624956cefe8774eafc1d5723409d77c464a887b8
Raul Alejandro Gonzalez <Nawn1994@gmail.com>
2016-04-10 22:45:15 -0700

    Playable game complete.

--- play.rb
+++ play.rb
@@ -8,26 +8,38 @@ end
 
 puts "Thanks for playing Chess!"
 
-select = false
-until select
-  puts "Please select the game mode! (Enter 1, 2, 3, or 4)\n\n1. Single player\n2. Multi player\n3. Load last save\n4. Exit"
+done = false
 
-  case menu_select(gets.chomp)
-  when 1
-    puts "Creating board with AI..."
-    board = Board.new(:singleplayer)
-    select = true
-  when 2
-    puts "Creating Board for 2 players..."
-    board = Board.new(:multiplayer)
-    select = true
-  when 3
-    puts "Save/Load not implemented yet"
-    select = true
-  when 4
-    exit
-    select = true
-  else
-    puts "Input not recognized\nReturning to menu"
+until done
+  select = false
+  until select
+    puts "Please select the game mode! (Enter 1, 2, or 3)\n\n1. Single player\n2. Load last save\n3. Exit"
+
+    case menu_select(gets.chomp)
+    when 1
+      puts "\n\nCreating Board for 2 players..."
+      board = Board.new(:multiplayer)
+      select = true
+    when 2
+      puts "Save/Load not implemented yet"
+      select = true
+    when 3
+      exit
+      select = true
+    else
+      puts "Input not recognized\nReturning to menu"
+    end
   end
+
+  until board.over?
+    board.turn
+  end
+
+  puts "\n\nGame ends in Draw! STALEMATE!\n\n"if board.players[0].stalemate?
+  puts "\n\nCHECKMATE! #{board.players[1].team_color.to_s.capitalize} Player wins!" if board.players[0].checkmate?
+  puts "\n\nDo you wish you play again? (Anything to continue, No to quit)"
+  response = gets.chomp.downcase
+  done = true if response == "no" || response == "n" || response == "exit"
 end
+
+puts "Thanks for playing!"
